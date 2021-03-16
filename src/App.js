2ad6d5c32be74f1a7fced0bbf40e25b472f0f6cd
Berker Buğur <bugur.berker@gmail.com>
2021-03-16 19:41:19 +0300

    initial commit for public repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.sass';
+import Home from "./components/pages/home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
